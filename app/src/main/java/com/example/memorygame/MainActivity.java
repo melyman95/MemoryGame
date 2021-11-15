9bca9531def3feb59f9af9b5a99ad8dbaad0708e
melyman95 <mattlyman12@gmail.com>
2021-11-14 22:15:11 -0800

    initial layout

--- app/src/main/java/com/example/memorygame/MainActivity.java
+++ app/src/main/java/com/example/memorygame/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.memorygame;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView brain = findViewById(R.id.brain);
+        int imageResource = getResources().getIdentifier("@drawable/brain",
+                null, this.getPackageName());
+        brain.setImageResource(imageResource);
     }
 }
