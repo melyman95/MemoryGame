829824addd6a648d9a7f5a1a7f5a276d9c5694fc
melyman95 <mattlyman12@gmail.com>
2021-11-17 18:30:33 -0800

    made timer, not functional yet

--- app/src/main/java/com/example/memorygame/EasyGame.java
+++ app/src/main/java/com/example/memorygame/EasyGame.java
@@ -3,10 +3,16 @@ package com.example.memorygame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -16,7 +22,12 @@ public class EasyGame extends AppCompatActivity {
 
     private int AMOUNT_OF_SQUARES = 9;
     private int TIME_LIMIT = 180;
+    private long timeLeftInMilliseconds;
+    private CountDownTimer gameTimer;
+    private Button startTimerButton;
+    private boolean isTimeLeft;
 
+    // constructors
     public EasyGame() {
 
     }
@@ -26,6 +37,7 @@ public class EasyGame extends AppCompatActivity {
         this.TIME_LIMIT = TIME_LIMIT;
     }
 
+    // getters and setters
     public Button[][] getGameButtons() {
         return gameButtons;
     }
@@ -55,8 +67,37 @@ public class EasyGame extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        Timer gameTimer = new Timer();
+        TextView timerText = findViewById(R.id.timer);
+
+        startTimerButton = findViewById(R.id.startTimerButton);
+
+        startTimerButton.setOnClickListener(new View.OnClickListener() {
+            /**
+             * Called when a view has been clicked.
+             *
+             * @param v The view that was clicked.
+             */
+            @Override
+            public void onClick(View v) {
+                startStop();
+            }
+        });
     }
 
+    public void startStop() {
+        if (isTimeLeft) {
+            stopTimer();
+        }
+        else{
+            startTimer();
+        }
+    }
 
+    public void startTimer() {
+
+    }
+
+    public void stopTimer() {
+
+    }
 }
